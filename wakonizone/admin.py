a8e5b48de6ec5a2386ed15d9457242385c981717
MugureNjunge <maureennjunge22@gmail.com>
2022-06-21 01:54:11 +0300

    register models in admin file

--- wakonizone/admin.py
+++ wakonizone/admin.py
@@ -1,8 +1,8 @@
-# from django.contrib import admin
-# from .models import *
+from django.contrib import admin
+from .models import *
 
-# # Register your models here.
-# admin.site.register(Business)
-# admin.site.register(Profile)
-# admin.site.register(Locality)
-# admin.site.register(Post)
+# Register your models here.
+admin.site.register(Business)
+admin.site.register(Profile)
+admin.site.register(Locality)
+admin.site.register(Post)
